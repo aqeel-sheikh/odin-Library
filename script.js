c05f0390f67597f18c4ffc5c921bf35c8dd4d63e
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-07-28 19:22:01 +0530

    add functionality to dynamically add books with title and author to the book Shelf

--- script.js
+++ script.js
@@ -6,18 +6,52 @@
 let myLibrary = [];
 
 function Book(bookName, bookAuthor, bookID) {
-  this.name = bookName
-  this.author = bookAuthor
-  this.ID = bookID
+  this.name = bookName;
+  this.author = bookAuthor;
+  this.ID = bookID;
 }
 
 function addBookToLibrary(bookName, bookAuthor) {
-    const uuid = crypto.randomUUID();
-    let newBook = new Book(bookName, bookAuthor, uuid)
-    myLibrary.push(newBook)
+  const uuid = crypto.randomUUID();
+  let newBook = new Book(bookName, bookAuthor, uuid);
+  myLibrary.push(newBook);
 }
-addBookToLibrary("book1","AKeel")
-addBookToLibrary("book2","leeka")
+
+const addBtn = document.querySelector("#add-btn");
+const bookShelf = document.querySelector(".book-shelf");
+
+addBtn.addEventListener("click", () => {
+  const newBookName = document.querySelector("#new-book-name").value;
+  const newBookAuthor = document.querySelector("#new-book-author").value;
+
+  if (!newBookName || !newBookAuthor) {
+    console.log("Cant log this book");
+    return;
+  }
+  addBookToLibrary(newBookName, newBookAuthor);
+
+  const newBook = document.createElement("div");
+  newBook.classList.add("book");
+
+  const bookName = document.createElement("div");
+  bookName.classList.add("book-info");
+
+  const bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book-info");
+
+  const delBtn = document.createElement("img");
+  delBtn.classList.add("del-btn");
+  delBtn.src = "/delete.svg";
+
+  newBook.append(bookName);
+  newBook.append(bookAuthor);
+  newBook.append(delBtn);
+
+  bookName.textContent = newBookName;
+  bookAuthor.textContent = newBookAuthor;
+
+  bookShelf.append(newBook);
+});
 
 // const div = document.querySelector(".library")
 // myLibrary.forEach(book => {
